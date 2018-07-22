42497964ea8294c7ba7ec62a7a8c479602ca1240
Benjamin <ben.smith.gordon@gmail.com>
2018-07-22 22:06:02 +0100

    refactored database switching methods

--- lib/book.rb
+++ lib/book.rb
@@ -2,26 +2,24 @@ require 'pg'
 
 class Book
 
-def self.all
-
-  if ENV['ENVIRONMENT'] == 'test'
-    connection = PG.connect(dbname: 'Rate_My_Book_test')
-  else
-    connection = PG.connect(dbname: 'Rate_My_Book')
-  end
-
-  result = connection.exec("SELECT * FROM books")
-  result.map { |book| book['title'] }
+  def self.all
+    Book.switch_database
+    result = @connection.exec("SELECT * FROM books")
+    result.map { |book| book['title'] }
   end
 
 
   def self.create(options)
-  if ENV['ENVIRONMENT'] == 'test'
-    connection = PG.connect(dbname: 'Rate_My_Book_test')
-  else
-    connection = PG.connect(dbname: 'Rate_My_Book')
+    Book.switch_database
+    @connection.exec("INSERT INTO books (title) VALUES('#{options[:title]}')")
   end
 
-  connection.exec("INSERT INTO books (title) VALUES('#{options[:title]}')")
+
+  def self.switch_database
+    if ENV['ENVIRONMENT'] == 'test'
+      @connection = PG.connect(dbname: 'Rate_My_Book_test')
+    else
+      @connection = PG.connect(dbname: 'Rate_My_Book')
+    end
   end
 end
